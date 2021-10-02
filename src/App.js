2f77b7993c8e3575df5b5380162078a4ba04a4aa
Ayo Abimbola <ayoinspired@gmail.com>
2021-10-02 22:08:46 +0100

    Sidebar item is styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
+			<div className="container flexed">
+				<Sidebar />
+
+				<div className="others">other pages</div>
+			</div>
 		</div>
 	);
 }
