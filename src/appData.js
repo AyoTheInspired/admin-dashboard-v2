03ee85ee307acbca3a132590ded893112e25b933
Ayo Abimbola <ayoinspired@gmail.com>
2021-10-04 09:17:40 +0100

    Featured Info is done

--- src/appData.js
+++ src/appData.js
@@ -51,3 +51,9 @@ export const sidebarItems = [
 		],
 	},
 ];
+
+export const featuredItems = [
+	{ id: Math.random(), title: "Revenue", amount: "2,415", margin: -11.4 },
+	{ id: Math.random(), title: "Sales", amount: "4,415", margin: -1.4 },
+	{ id: Math.random(), title: "Cost", amount: "2,225", margin: "+2.4" },
+];
--- src/components/FeaturedInfo.jsx
+++ src/components/FeaturedInfo.jsx
@@ -1,26 +1,83 @@
 import React from "react";
-import { ArrowDownward } from "@material-ui/icons";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 import styled from "styled-components";
+import { featuredItems } from "../appData";
 
 function FeaturedInfo() {
 	return (
 		<Div>
-			<div className="featuredItem">
-				<span className="featuredTitle">Revenue</span>
-				<div className="featuredMoneyContainer">
-					<span className="featuredMoney">$ 2,415</span>
-					<span className="featuredMoneyRate">
-						{" "}
-						-11.4 <ArrowDownward />
-					</span>
+			{featuredItems.map(({ id, title, amount, margin }) => (
+				<div className="featuredItem" key={id}>
+					<span className="featuredTitle">{title}</span>
+					<div className="featuredMoneyContainer">
+						<span className="featuredMoney">$ {amount}</span>
+						<span className="featuredMoneyRate">
+							{margin}{" "}
+							<span
+								className={`${
+									margin < 0 ? "negative" : "positive"
+								} featuredIcon`}>
+								{margin < 0 ? <ArrowDownward /> : <ArrowUpward />}{" "}
+							</span>
+						</span>
+					</div>
+					<span className="featuredSub">Compared to last month</span>
 				</div>
-
-				<span className="featuredSub">Compared to last month</span>
-			</div>
+			))}
 		</Div>
 	);
 }
 
 export default FeaturedInfo;
 
-const Div = styled.div``;
+const Div = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+
+	.featuredItem {
+		flex: 1;
+		margin: 0px 20px;
+		padding: 30px;
+		border-radius: 10px;
+		cursor: pointer;
+		box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+	}
+
+	.featuredTitle {
+		font-size: 20px;
+	}
+
+	.featuredMoneyContainer {
+		margin: 10px 0px;
+		margin: 10px;
+		display: flex;
+		align-items: center;
+	}
+
+	.featuredMoney {
+		font-size: 25px;
+		font-weight: 600;
+	}
+
+	.featuredMoneyRate {
+		display: flex;
+		align-items: center;
+		margin-left: 20px;
+	}
+
+	.featuredIcon {
+		font-size: 10px;
+		margin-left: 5px;
+		color: green;
+
+		&.negative {
+			color: red;
+		}
+	}
+
+	.featuredSub {
+		font-size: 15px;
+		color: grey;
+	}
+`;
