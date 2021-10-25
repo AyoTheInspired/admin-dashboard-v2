5324838295dd489ae23210bd622b62fd7ac6ac5f
AyoTheInspired <ayoinspired@gmail.com>
2021-10-25 10:17:43 +0100

    ADDED: Delete users from users page

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,13 +1,69 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import { DeleteOutline } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
-import { usersColumns, usersRows } from "../app-data/users-list";
+import { usersRows } from "../app-data/users-list";
 
 function UserList() {
+	const [data, setData] = useState(usersRows);
+
+	const handleDelete = (id) => {
+		setData(data.filter((item) => item.id !== id));
+	};
+
+	const usersColumns = [
+		{ field: "id", headerName: "ID", width: 90 },
+		{
+			field: "avatar",
+			headerName: "User",
+			renderCell: (params) => (
+				<div className="userAvtContaner flexed">
+					<img
+						src={params.row.avatar}
+						alt={params.row.username}
+						className="userAvt"
+					/>
+					{params.row.username}
+				</div>
+			),
+			width: 180,
+		},
+		{ field: "email", headerName: "Email", width: 230 },
+		{
+			field: "status",
+			headerName: "Status",
+			width: 120,
+		},
+		{
+			field: "transaction",
+			headerName: "Transaction Amount",
+			width: 200,
+		},
+
+		{
+			field: "action",
+			headerName: "Actions",
+			sortable: false,
+			width: 150,
+			renderCell: (params) => (
+				<div className="flex-btw">
+					<Link to={`/user/${params.row.id}`}>
+						<button className="userEditBtn">Edit</button>
+					</Link>
+					<DeleteOutline
+						className="userDeleteBtn"
+						onClick={() => handleDelete(params.row.id)}
+					/>
+				</div>
+			),
+		},
+	];
+
 	return (
 		<Div>
 			<DataGrid
-				rows={usersRows}
+				rows={data}
 				columns={usersColumns}
 				disableSelectionOnClick
 				pageSize={10}
@@ -22,7 +78,7 @@ export default UserList;
 
 const Div = styled.div`
 	flex: 4;
-	padding: 25px 10px;
+	padding: 25px 10px 0px 10px;
 
 	.userAvt {
 		width: 40px;
